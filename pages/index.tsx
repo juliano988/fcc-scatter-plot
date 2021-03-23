93b9cd1121085f7508678e55d911f97fdc67fb94
Júlio Faria <julio-pinheiroo@hotmail.com>
2021-03-23 17:05:00 -0300

    Dia 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import * as d3 from "d3";
 import styles from '../styles/Home.module.css'
 import { GraphicProps } from '../customInterfaces'
-import { ticks } from 'd3';
+import tippy, { Instance, Props } from 'tippy.js';
+import ReactDOMServer from 'react-dom/server';
 
 export default function Home({ data }): JSX.Element {
   return (
@@ -23,10 +24,13 @@ export default function Home({ data }): JSX.Element {
 
 function Graphic(props: GraphicProps): JSX.Element {
 
+  const [forceRender, setForceRender] = useState<number>(Math.random())
+
   const graphicDivRef = useRef<HTMLDivElement>(null);
 
-  const offset = 35
-  const circlesRadius = 5
+  const offset = 25;
+  const leftLabelWidth = 45;
+  const circlesRadius = 5;
 
   useEffect(function () {
 
@@ -46,36 +50,60 @@ function Graphic(props: GraphicProps): JSX.Element {
 
     const svg = d3.select(graphicDivRef.current)
       .append('svg')
-      .attr('width', svgWidth + offset * 2)
-      .attr('height', svgHeight + offset * 2)
+      .attr('width', svgWidth + offset * 2 + leftLabelWidth)
+      .attr('height', svgHeight + offset * 3)
 
     const xAxis = svg.append('g')
       .call(d3.axisTop(scaleX).tickFormat(d3.format(".0f"))
         .tickSize(-svgHeight)
-        .tickPadding(10))
-      .attr('transform', 'translate(' + offset + ',' + offset + ')')
-      .attr("stroke-dasharray", "2")
-      .attr("stroke-opacity", 0.5)
+        .tickPadding(5))
+      .attr('transform', 'translate(' + (offset + leftLabelWidth) + ',' + offset + ')')
+      .attr('stroke-dasharray', "2")
+      .attr('stroke-opacity', 0.5)
+      .attr('font-size', '1rem')
       .select('path').remove()
-
-    let teste = [...props.data.map(function (val) { return val.Time.replace(':', '') })]
-    console.log(teste)
 
     const yAxis = svg.append('g')
       .call(d3.axisLeft(d3.scaleTime().domain([minSecDate, maxSecDate]).range([circlesRadius, svgHeight - circlesRadius]))
-        .tickFormat((d: Date) => d.getMinutes() + ':' + d.getSeconds().toString(10).padStart(2, '0'))
+        .tickFormat((d: Date) => d.getMinutes() + ':' + d.getSeconds().toString(10).padStart(2, '0') + 'min')
         .ticks(d3.timeSecond.every(30))
         .tickSize(-svgWidth)
-        .tickPadding(10))
-      .attr('transform', 'translate(' + offset + ',' + offset + ')')
+        .tickPadding(5))
+      .attr('transform', 'translate(' + (offset + leftLabelWidth) + ',' + offset + ')')
       .attr("stroke-dasharray", "2")
       .attr("stroke-opacity", 0.5)
+      .attr('font-size', '1rem')
       .select('path').remove()
 
-    console.log(minSecDate.getMinutes())
+    const subtitle = svg.append('g')
+      .attr('transform', 'translate(' + (leftLabelWidth + svgWidth - 210) + ',' + (svgHeight + offset + 10) + ')')
+
+    subtitle.append('text')
+      .style('font-size', '1rem')
+      .attr('transform', 'translate(0,13)')
+      .text('Riders with doping allegation')
+
+    subtitle.append('rect')
+      .attr('width', 15)
+      .attr('height', 15)
+      .attr('fill', 'orange')
+      .style('stroke', 'black')
+      .attr('transform', 'translate(210,0)')
+
+    subtitle.append('text')
+      .style('font-size', '1rem')
+      .attr('transform', 'translate(50,33)')
+      .text('No doping allegations')
+
+    subtitle.append('rect')
+      .attr('transform', 'translate(210,20)')
+      .attr('width', 15)
+      .attr('height', 15)
+      .attr('fill', 'blue')
+      .style('stroke', 'black')
 
     const circles = svg.append('g')
-      .attr('transform', 'translate(' + offset + ',' + offset + ')')
+      .attr('transform', 'translate(' + (offset + leftLabelWidth) + ',' + offset + ')')
       .selectAll('circle')
       .data(props.data).enter()
       .append('circle')
@@ -84,7 +112,36 @@ function Graphic(props: GraphicProps): JSX.Element {
       .attr('cx', (d) => scaleX(d.Year))
       .attr('cy', (d) => scaleY(d.Seconds))
 
-    console.log(props.data)
+    const tippyInstanceArr: Array<Instance<Props>> = [];
+    props.data.forEach(function (infos, i) {
+      tippyInstanceArr.push(tippy((svg.selectAll('circle').nodes()[i] as SVGAElement), {
+        allowHTML: true,
+        placement: 'right-end',
+        content: ReactDOMServer.renderToStaticMarkup(
+          <div style={{ textAlign: 'left', fontSize: '1rem', backgroundColor: (infos.Doping ? 'hsla(40, 100%, 50%, 0.8)' : 'hsla(250, 100%, 50%, 0.8)'), maxWidth: '300px', padding: '3px', borderRadius: '3px' }}>
+            <p style={{ margin: '0px' }}><strong>Name: </strong>{infos.Name}, {infos.Nationality}</p>
+            <p style={{ margin: '0px' }}><strong>Placement: </strong>{infos.Place}º, {infos.Year}</p>
+            <p style={{ margin: '0px' }}><strong>Time: </strong>{infos.Time}min</p>
+            {infos.Doping && <p style={{ margin: '0px' }}>{infos.Doping}</p>}
+          </div>
+        )
+      }));
+    })
+
+    return function () {
+      tippyInstanceArr.map(function (elem) { return elem.unmount() })
+      svg.remove();
+    }
+  }, [forceRender])
+
+  useEffect(function () {
+    function forceRenderFunc() {
+      setForceRender(Math.random())
+    }
+    window.addEventListener('resize', forceRenderFunc)
+    return function () {
+      window.removeEventListener('resize', forceRenderFunc)
+    }
   }, [])
 
   return (
